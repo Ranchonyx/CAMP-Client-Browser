9ac401c2ba5be7e406e68908c681ab0a6f7d72b9
Ranchonyx <felixjanetzki@gmail.com>
2026-08-12 10:47:41 +0200

    re-rexport for CAMPBuffer and CAMPReadables

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import {CAMPClientWebsocketSession} from "./CAMPClientWebsocketSession/CAMPClientWebsocketSession.js";
+import {CAMPReadable} from "./Common/Wrappers/CAMPReadable.js";
+import {CAMPBuffer} from "camp-protocol";
 
 /**
  * Create a CAMP client
@@ -6,6 +8,8 @@ import {CAMPClientWebsocketSession} from "./CAMPClientWebsocketSession/CAMPClien
  * @param bearer - The Bearer token for the server to validate
  * @param timeout - How long to wait until the client stops establishing a connection
  * */
-export async function camp(host: string, bearer: string, timeout: number = 5000) {
+async function camp(host: string, bearer: string, timeout: number = 5000) {
     return CAMPClientWebsocketSession.Connect(host, bearer, timeout);
 }
+
+export {CAMPReadable, CAMPBuffer, camp};
